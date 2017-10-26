79b4f18c54b6087406f6e24f7dfda37b2bc46051
Sputnik <stevena7@gmail.com>
2017-10-26 22:57:55 +1100

    Add time date inputs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {compose, withState} from 'recompose';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Spooky Timer</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const App = ({
+  time,
+  updateTime,
+  date,
+  updateDate
+}) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to Spooky Timer</h1>
+      </header>
+      <p className="App-intro">
+        To get started, pick a time.
+      </p>
+      <p>
+        <input
+          type="time"
+          placeholder="time"
+          value={time}
+          onChange={(e) => updateTime(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="date"
+          value={date}
+          onChange={(e) => updateDate(e.target.value)}
+        />
+
+      </p>
+      <p>
+        {time} + {date}
+      </p>
+    </div>
+  );
 }
 
-export default App;
+export default compose (
+  withState('time', 'updateTime', ""),
+  withState('date', 'updateDate', ""),
+)(App);
