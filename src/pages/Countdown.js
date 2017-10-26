fa77786c7fd867c1ffd6e4a0ab50c70f375697a8
Sputnik <stevena7@gmail.com>
2017-10-27 01:00:08 +1100

    Add spooky animation to timer page

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -2,13 +2,48 @@ import React from 'react';
 import Countdown from 'react-countdown-now';
 import {compose} from 'recompose';
 import {withRouter} from 'react-router-dom';
+import injectSheet from 'react-jss';
 
-const count = ({location}) => (
-  <Countdown date={parseInt(location.search.slice(6), 10)} />
+const styles = {
+  '@keyframes enter': {
+    '0%': {
+      backgroundColor: '#ffffff',
+      color: '#ffffff'
+    },
+    '50%': {
+      backgroundColor: '#000000',
+      color: '#000000'
+    },
+    '100%': {
+      backgroundColor: '#000000',
+      color: '#666666'
+    }
+  },
+  countdown: {
+    position: 'absolute',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#000000',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+
+    animation: 'enter 5s forwards'
+  }
+}
+
+const count = ({classes, location}) => (
+  <div className={classes.countdown}>
+    <h1>Spooky Timer</h1>
+    <Countdown date={parseInt(location.search.slice(6), 10)} />
+  </div>
 )
 
 const Count = compose(
-  withRouter
+  withRouter,
+  injectSheet(styles)
 )(count);
 
 export default Count;
